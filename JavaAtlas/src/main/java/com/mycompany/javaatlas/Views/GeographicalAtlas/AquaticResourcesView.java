e5a0cf0d74fb3507368e0907f42fc0ff0f8331f5
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-09-26 22:55:06 +0300

    Create Oceans view

--- JavaAtlas/src/main/java/com/mycompany/javaatlas/Views/GeographicalAtlas/AquaticResourcesView.java
+++ JavaAtlas/src/main/java/com/mycompany/javaatlas/Views/GeographicalAtlas/AquaticResourcesView.java
@@ -2,21 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.javaatlas.Views;
+package com.mycompany.javaatlas.Views.GeographicalAtlas;
 
-import com.mycompany.javaatlas.MainForm;
+import com.mycompany.javaatlas.Views.GeographicalAtlas.AquaticResources.*;
+import com.mycompany.javaatlas.Views.GeographicalAtlasView;
 
 /**
  *
  * @author user
  */
-public class GeographicalAtlasView extends javax.swing.JFrame {
-     private String _address = "D:/Projects/JavaProjects/JavaAtlas/Sources/";
-     
+public class AquaticResourcesView extends javax.swing.JFrame {
+
     /**
-     * Creates new form GeographicalAtlasView
+     * Creates new form AquaticResourcesView
      */
-    public GeographicalAtlasView() {
+    public AquaticResourcesView() {
         initComponents();
     }
 
@@ -30,23 +30,23 @@ public class GeographicalAtlasView extends javax.swing.JFrame {
     private void initComponents() {
 
         title = new javax.swing.JLabel();
-        aquaticResources = new javax.swing.JButton();
+        oceans = new javax.swing.JButton();
         back = new javax.swing.JButton();
-        icon = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(600, 490));
 
         title.setFont(new java.awt.Font("Times New Roman", 1, 36)); // NOI18N
-        title.setText("Географічний атлас світу");
+        title.setText("Водні ресурси на планеті Земля");
 
-        aquaticResources.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        aquaticResources.setText("Водні ресурси");
-        aquaticResources.addActionListener(new java.awt.event.ActionListener() {
+        oceans.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        oceans.setText("Океани");
+        oceans.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                aquaticResourcesActionPerformed(evt);
+                oceansActionPerformed(evt);
             }
         });
+
+        
 
         back.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         back.setText("Назад");
@@ -56,57 +56,57 @@ public class GeographicalAtlasView extends javax.swing.JFrame {
             }
         });
 
-
-        icon.setIcon(new javax.swing.ImageIcon(_address + "Images/GeographicalAtlas.jpg"));
+        
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(100, 100, 100)
-                .addComponent(title)
-                .addContainerGap(42, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(22, 22, 22)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(aquaticResources, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(land, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(title)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(53, 53, 53)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(33, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(icon)
-                    .addContainerGap(554, Short.MAX_VALUE)))
         );
-
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(title, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(oceans)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(back, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void aquaticResourcesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aquaticResourcesActionPerformed
+    private void oceansActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_oceansActionPerformed
         // TODO add your handling code here:
         this.setVisible(false);
-        //Vodni_resursi ok = new Vodni_resursi();
-        //ok.setVisible(true);
-    }//GEN-LAST:event_aquaticResourcesActionPerformed
+        var oceans = new Oceans();
+        oceans.setVisible(true);
+    }//GEN-LAST:event_oceansActionPerformed
+
+   
 
     private void backActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backActionPerformed
         // TODO add your handling code here:
         this.setVisible(false);
-        var mainForm = new MainForm();
-        mainForm.setVisible(true);
+        var geographicalAtlas = new GeographicalAtlasView();
+        geographicalAtlas.setVisible(true);
     }//GEN-LAST:event_backActionPerformed
-
-    private void landActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_landActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-    }//GEN-LAST:event_landActionPerformed
 
     /**
      * @param args the command line arguments
@@ -125,28 +125,33 @@ public class GeographicalAtlasView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GeographicalAtlasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AquaticResourcesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GeographicalAtlasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AquaticResourcesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GeographicalAtlasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AquaticResourcesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GeographicalAtlasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AquaticResourcesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GeographicalAtlasView().setVisible(true);
+                new AquaticResourcesView().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton aquaticResources;
     private javax.swing.JButton back;
-    private javax.swing.JLabel icon;
+    private javax.swing.JButton bays;
+    private javax.swing.JButton lakes;
+    private javax.swing.JButton oceans;
+    private javax.swing.JButton rivers;
+    private javax.swing.JButton seas;
+    private javax.swing.JButton straits;
     private javax.swing.JLabel title;
+    private javax.swing.JButton waterfalls;
     // End of variables declaration//GEN-END:variables
 }
